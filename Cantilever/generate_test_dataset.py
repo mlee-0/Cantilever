c7f6f63110d03a9a7782d5c65f7cde35d0e7f632
Marshall Lee <marsh92909@gmail.com>
2021-11-12 23:24:23 -0500

    Modify CNN, add dataset images
    - Added 1 more conv. layer in CNN
    - Increased epochs 30 -> 40
    - Changed test values

--- Cantilever/generate_test_dataset.py
+++ Cantilever/generate_test_dataset.py
@@ -2,5 +2,8 @@ from main import generate_input_images, FOLDER_TEST_INPUTS
 
 
 # Generate new data not part of the training dataset.
-test_angles = [1, 7, 13, 43, 82]
-test_filenames = generate_input_images(test_angles, FOLDER_TEST_INPUTS)
+test_loads = [25000, 50000, 75000, 100000]
+test_angles = [5, 30, 90, 175]
+test_lengths = [3] * 4
+test_heights = [1.5] * 4
+test_filenames = generate_input_images((test_loads, test_angles, test_lengths, test_heights), FOLDER_TEST_INPUTS)
