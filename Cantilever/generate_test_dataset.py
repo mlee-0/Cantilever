2487c4dda1d6be251f9d791eec9cef384e020e02
Marshall Lee <marsh92909@gmail.com>
2021-11-18 15:56:13 -0500

    Modify CNN, remove scaling stress to [0,1] in csv
    - Added samples txt file for test data
        - Test script now follows same procedure as train
    - Scale stress values in Dataset in __init__
        - Nan loss values if no scaling
        - This allows test outputs to be compared directly to FEA data
        - Scale test outputs to original range of stress values found in csv
    - Added deconv layers to CNN
    - Renamed sample files to have "train/test" at end of name

--- Cantilever/generate_test_dataset.py
+++ Cantilever/generate_test_dataset.py
@@ -1,13 +1,26 @@
-from main import FOLDER_TEST_INPUTS, generate_samples, generate_input_images, calculate_load_components, write_ansys_script
+from main import FOLDER_TEST_INPUTS, FOLDER_TEST_OUTPUTS, generate_samples, generate_input_images, calculate_load_components, write_ansys_script, write_fea_spreadsheet
+from generate_train_dataset import write_samples, read_samples
 
+NUMBER_SAMPLES = 8
+FILENAME_SAMPLES = 'samples_test.txt'
 
 if __name__ == '__main__':
-    # Generate new data not part of the training dataset.
-    test_loads = [90000] * 4 + [30000, 50000, 70000, 90000]
-    test_angles = [5, 10, 15, 30] + [270] * 4
-    test_lengths = [3] * 8
-    test_heights = [1.5] * 8
-    samples = (test_loads, test_angles, test_lengths, test_heights)
+    # Try to read sample values from the text file if it already exists. If not, generate the samples.
+    samples = read_samples(FILENAME_SAMPLES)
+    if not samples:
+        samples = generate_samples(NUMBER_SAMPLES, show_histogram=False)
     load_components = calculate_load_components(*samples[0:2])
+    write_samples(samples, load_components, FILENAME_SAMPLES)
     write_ansys_script(samples, load_components, 'ansys_script_test.lgw')
     generate_input_images(samples, FOLDER_TEST_INPUTS)
+    write_fea_spreadsheet(samples, FOLDER_TEST_OUTPUTS, 'stress.csv')
+    # # Generate new data not part of the training dataset.
+    # # test_loads = [90000] * 4 + [30000, 50000, 70000, 90000]
+    # # test_angles = [5, 10, 15, 30] + [270] * 4
+    # # test_lengths = [3] * 8
+    # # test_heights = [1.5] * 8
+    # # samples = (test_loads, test_angles, test_lengths, test_heights)
+    # samples = generate_samples(5, show_histogram=False)
+    # load_components = calculate_load_components(*samples[0:2])
+    # write_ansys_script(samples, load_components, 'ansys_script_test.lgw')
+    # generate_input_images(samples, FOLDER_TEST_INPUTS)
