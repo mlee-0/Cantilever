8e80a74f8b044ccc8cf15369a2701ceb671a6d87
Marshall Lee <marsh92909@gmail.com>
2022-02-10 20:01:43 -0500

    Adjust networks and remove BaseCnn class
    - Added dropout in FullyCnn, AutoencoderCnn

--- Cantilever/networks.py
+++ Cantilever/networks.py
@@ -11,43 +11,40 @@ from torch import nn
 from setup import INPUT_CHANNELS, INPUT_SIZE, OUTPUT_CHANNELS, OUTPUT_SIZE
 
 
-class BaseCnn(nn.Module):
-    """Base class with common helper methods used by all CNN classes."""
+# Return a sequence of layers related to convolution.
+def convolution(in_channels, out_channels, **kwargs):
+    return nn.Sequential(
+        nn.Conv2d(in_channels=in_channels, out_channels=out_channels, **kwargs, padding_mode='replicate'),
+        nn.BatchNorm2d(out_channels),
+        nn.ReLU(inplace=True),
+        )
 
-    # Return a sequence of layers related to convolution.
-    @staticmethod
-    def convolution(in_channels, out_channels, **kwargs):
-        return nn.Sequential(
-            nn.Conv2d(in_channels=in_channels, out_channels=out_channels, **kwargs, padding_mode='replicate'),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True),
-            )
-    
-    # Return a sequence of layers related to deconvolution.
-    @staticmethod
-    def deconvolution(in_channels, out_channels, **kwargs):
-        return nn.Sequential(
-            nn.ConvTranspose2d(in_channels=in_channels, out_channels=out_channels, **kwargs),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True),
-            )
+# Return a sequence of layers related to deconvolution.
+def deconvolution(in_channels, out_channels, **kwargs):
+    return nn.Sequential(
+        nn.ConvTranspose2d(in_channels=in_channels, out_channels=out_channels, **kwargs),
+        nn.BatchNorm2d(out_channels),
+        nn.ReLU(inplace=True),
+        )
 
-class FullyCnn(BaseCnn):
+
+class FullyCnn(nn.Module):
     def __init__(self):
         super().__init__()
-        self.convolution_1 = self.convolution(
+        self.convolution_1 = convolution(
             in_channels=INPUT_CHANNELS, out_channels=64, kernel_size=3, stride=2, padding=1,
             )
-        self.convolution_2 = self.convolution(
+        self.convolution_2 = convolution(
             in_channels=64, out_channels=128, kernel_size=3, stride=2, padding=1,
             )
-        self.convolution_3 = self.convolution(
+        self.convolution_3 = convolution(
             in_channels=128, out_channels=256, kernel_size=3, stride=2, padding=1,
             )
-        # self.convolution_4 = self.convolution(
+        # self.convolution_4 = convolution(
         #     in_channels=32, out_channels=64, kernel_size=3, stride=3, padding=1,
         #     )
         self.pooling = nn.MaxPool2d(kernel_size=2, stride=2, return_indices=True)
+        self.dropout = nn.Dropout(0.5)
 
         self.linear = nn.Linear(in_features=23296, out_features=np.prod(OUTPUT_SIZE))
 
@@ -55,13 +52,15 @@ class FullyCnn(BaseCnn):
         x = x.float()
         # Convolution.
         x = self.convolution_1(x)
+        x = self.dropout(x)
         x = self.convolution_2(x)
+        x = self.dropout(x)
         x = self.convolution_3(x)
+        x = self.dropout(x)
         # x = self.convolution_4(x)
         # Fully connected.
         x = x.view(x.size(0), -1)
         x = self.linear(x)
-
         x = x.reshape(-1, *OUTPUT_SIZE)
         return x
 
@@ -163,22 +162,22 @@ class UNet(nn.Module):
         final_layer = self.final_layer(decode_block1)
         return final_layer
 
-class UNetCnn(BaseCnn):
+class UNetCnn(nn.Module):
     def __init__(self):
         super().__init__()
-        self.convolution_1 = self.convolution(
+        self.convolution_1 = convolution(
             in_channels=INPUT_CHANNELS, out_channels=8, kernel_size=3, stride=3, padding=1,
             )
-        self.convolution_2 = self.convolution(
+        self.convolution_2 = convolution(
             in_channels=8, out_channels=16, kernel_size=3, stride=3, padding=1,
             )
-        self.deconvolution_1 = self.deconvolution(
+        self.deconvolution_1 = deconvolution(
             in_channels=16, out_channels=8, kernel_size=7, stride=3, padding=1,
             )
-        self.deconvolution_2 = self.deconvolution(
+        self.deconvolution_2 = deconvolution(
             in_channels=8, out_channels=4, kernel_size=7, stride=3, padding=1,
             )
-        self.deconvolution_3 = self.deconvolution(
+        self.deconvolution_3 = deconvolution(
             in_channels=4, out_channels=OUTPUT_CHANNELS, kernel_size=7, stride=3, padding=1,
             )
         self.pooling = nn.MaxPool2d(kernel_size=2, stride=2, return_indices=True)
@@ -210,59 +209,83 @@ class UNetCnn(BaseCnn):
         x = x.reshape(-1, *OUTPUT_SIZE)
         return x
 
-class AutoencoderCnn(BaseCnn):
+class AutoencoderCnn(nn.Module):
     def __init__(self):
         super().__init__()
         
-        self.convolution_1 = self.convolution(
-            in_channels=INPUT_CHANNELS, out_channels=8, kernel_size=3, stride=3, padding=0
+        self.convolution_1 = convolution(
+            in_channels=INPUT_CHANNELS, out_channels=64, kernel_size=3, stride=3, padding=0
             )
-        self.convolution_2 = self.convolution(
-            in_channels=8, out_channels=16, kernel_size=3, stride=3, padding=1
+        self.convolution_2 = convolution(
+            in_channels=64, out_channels=128, kernel_size=3, stride=3, padding=1
             )
-        self.deconvolution_1 = self.deconvolution(
-            in_channels=16, out_channels=8, kernel_size=3, stride=3, padding=1, output_padding=(1,0)
+        self.deconvolution_1 = deconvolution(
+            in_channels=128, out_channels=64, kernel_size=3, stride=3, padding=1, output_padding=(2,0)
             )
-        self.deconvolution_2 = self.deconvolution(
-            in_channels=8, out_channels=OUTPUT_CHANNELS, kernel_size=3, stride=3, padding=0, output_padding=(0,1)
+        self.deconvolution_2 = deconvolution(
+            in_channels=64, out_channels=OUTPUT_CHANNELS, kernel_size=3, stride=3, padding=0, output_padding=(2,1)
             )
         self.pooling = nn.MaxPool2d(kernel_size=2, stride=2, return_indices=True)
         self.unpooling = nn.MaxUnpool2d(kernel_size=2, stride=2)
-        self.dropout = nn.Dropout(0.8)
+        self.dropout = nn.Dropout(0)
+        self.linear = nn.Linear(in_features=5000, out_features=5000)
         
         self.flatten = nn.Flatten()
         self.autoencoder = nn.Sequential(
-            nn.Linear(in_features=48, out_features=24),
+            # nn.ReLU(),
+            nn.Linear(in_features=384, out_features=24),
             nn.Linear(in_features=24, out_features=12),
+            # nn.Linear(in_features=32, out_features=16),
+            # nn.Linear(in_features=12, out_features=6),
+            # nn.Linear(in_features=6, out_features=3),
+            # nn.Linear(in_features=3, out_features=6),
+            # nn.Linear(in_features=6, out_features=12),
+            # nn.Linear(in_features=16, out_features=32),
             nn.Linear(in_features=12, out_features=24),
-            nn.Linear(in_features=24, out_features=48),
+            nn.Linear(in_features=24, out_features=384),
+            # nn.ReLU(),
             )
-        self.unflatten = nn.Unflatten(1, (16, 1, 3))
+        # self.unflatten = nn.Unflatten(1, (128, 1, 3))
 
     def forward(self, x):
         x = x.float()
-        x = nn.functional.pad(x, (0, 1, 1, 1))  # Add one row to bottom, and one column on both left and right
+        # # Pad the bottom, left, and right to make the dimensions divisible by the stride.
+        # x = nn.functional.pad(x, (0, 1, 1, 1))
         # Convolution.
         x = self.convolution_1(x)
         x = self.dropout(x)
         size_1 = x.size()
+        # print(size_1)
         x, indices_1 = self.pooling(x)
+        # print(x.size())
         x = self.convolution_2(x)
         x = self.dropout(x)
         size_2 = x.size()
+        # print(size_2)
         x, indices_2 = self.pooling(x)
+        # print(x.size())
         # Autoencoding.
         size_encoding = x.size()
-        x = self.flatten(x)
-        x = self.autoencoder(x)
-        x = self.unflatten(x)  #x = x.reshape(size_encoding)
+        # x = self.flatten(x)
+        x = self.autoencoder(x.flatten())
+        x = x.reshape(size_encoding)  #x = self.unflatten(x)
         # Deconvolution.
+        # print(x.size())
         x = self.unpooling(x, indices_2, output_size=size_2)
+        # print(x.size())
         x = self.deconvolution_1(x)
         x = self.dropout(x)
+        x = x[..., :-1, :]
+        # print(x.size())
         x = self.unpooling(x, indices_1, output_size=size_1)
+        # print(x.size())
         x = self.deconvolution_2(x)
         x = self.dropout(x)
-        x = x[..., :-1, :]  # Remove last row added before first convolution
+        # print(x.size())
+        # x = x[..., :-1, :]  # Remove last row added before first convolution
+        
+        # x = x.view(x.size(0), -1)
+        # x = self.linear(x)
+        # x = x.reshape((1, *OUTPUT_SIZE))
 
         return x
