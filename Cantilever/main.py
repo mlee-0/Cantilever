Fix output quality by not scaling stress to [0,1]
- Reduced input image size (250,500) -> (50,100)

Cantilever/main.py
@@ -41,8 +41,8 @@ from setup import *
 FILEPATH_MODEL = os.path.join(FOLDER_ROOT, 'model.pth')
 # Training hyperparameters.
 BATCH_SIZE = 1
-LEARNING_RATE = 0.1
-EPOCHS = 10
+LEARNING_RATE = 0.00001
+EPOCHS = 100
 
 
 class CantileverDataset(Dataset):
@@ -61,6 +61,7 @@ class CantileverDataset(Dataset):
         self.labels, maxima = generate_label_images(
             samples,
             FOLDER_TRAIN_OUTPUTS if is_train else FOLDER_TEST_OUTPUTS,
+            normalize=False,
             normalization_values=CantileverDataset.maxima,  #(1100000, None),  # Manually selected value,
             clip_high_stresses=False,
             )
@@ -195,12 +196,15 @@ if __name__ == '__main__':
         test_output = test_output.detach().numpy()[0, :, ...]
         label = label[0, :, ...].numpy()
         
+        # The maximum stress found in the dataset, a hardcoded value that changes if a new dataset is generated.
+        MAX_STRESS = 26060.0
         for channel in range(OUTPUT_CHANNELS):
             channel_name = ('stress', 'displacement')[channel]
             # Write FEA images.
-            with Image.fromarray((array_to_colormap(label[channel, ...])).astype(np.uint8)) as image:
-                filepath = os.path.join(FOLDER_TEST_OUTPUTS, f'fea_{channel_name}_{i+1}.png')
-                image.save(filepath)
+            write_image(
+                array_to_colormap(label[channel, ...], MAX_STRESS),
+                os.path.join(FOLDER_TEST_OUTPUTS, f'fea_{channel_name}_{i+1}.png'),
+                )
             # Evaluate outputs with multiple evaluation metrics.
             evaluation_result = metrics.evaluate(test_output[channel, ...], label[channel, ...])
             for name, result in evaluation_result.items():
@@ -209,11 +213,10 @@ if __name__ == '__main__':
                 except KeyError:
                     evaluation_results[channel][name] = [result]
             # Save the output image.
-            with Image.fromarray(array_to_colormap(test_output[channel, ...]).astype(np.uint8)) as image:
-                image.save(os.path.join(
-                    FOLDER_TEST_OUTPUTS,
-                    f'test_{channel_name}_{str(i+1).zfill(NUMBER_DIGITS)}.png',
-                    ))
+            write_image(
+                array_to_colormap(test_output[channel, ...], MAX_STRESS),
+                os.path.join(FOLDER_TEST_OUTPUTS, f'test_{channel_name}_{str(i+1).zfill(NUMBER_DIGITS)}.png'),
+                )
     print(f'Wrote {len(test_dataloader)} output images and {len(test_dataloader)} corresponding labels in {FOLDER_TEST_OUTPUTS}.')
 
     # Plot evaluation metrics.

Cantilever/networks.py
@@ -47,7 +47,7 @@ class FullyCnn(nn.Module):
             in_channels=32, out_channels=64, kernel_size=3, stride=2, padding=1,
             )
         self.pooling = nn.MaxPool2d(kernel_size=2, stride=2, return_indices=False)
-        self.linear = nn.Linear(in_features=8192, out_features=np.prod(OUTPUT_SIZE))
+        self.linear = nn.Linear(in_features=1456, out_features=np.prod(OUTPUT_SIZE))
 
     def forward(self, x):
         x = x.float()
@@ -55,13 +55,38 @@ class FullyCnn(nn.Module):
         x = self.convolution_1(x)
         x = self.convolution_2(x)
         x = self.convolution_3(x)
-        x = self.convolution_4(x)
-        x = self.convolution_5(x)
+        # x = self.convolution_4(x)
+        # x = self.convolution_5(x)
         # Fully connected.
         x = x.view(x.size(0), -1)
         x = self.linear(x)
         x = x.reshape(-1, *OUTPUT_SIZE)
         return x
+
+class Nie(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.convolution_1 = nn.Sequential(
+            nn.Conv2d(INPUT_CHANNELS, 32, kernel_size=9, stride=1, padding='same'),
+            nn.BatchNorm2d(32),
+            nn.ReLU(inplace=True),
+        )
+        self.convolution_2 = nn.Sequential(
+            nn.Conv2d(32, 64, kernel_size=3, stride=2, padding='same'),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+        )
+        self.convolution_3 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=3, stride=2, padding='same'),
+            nn.BatchNorm2d(32),
+            nn.ReLU(inplace=True),
+        )
+    
+    def forward(self, x):
+        self.convolution_1(x)
+        self.convolution_2(x)
+        self.convolution_3(x)
 
 class UNetCnn(nn.Module):
     def __init__(self):
@@ -101,7 +126,6 @@ class UNetCnn(nn.Module):
         # x = self.unpooling(x, indices_1, output_size=size_1)
         x = self.deconvolution_2(x)
         x = self.deconvolution_3(x)
-        
 
         # Fully connected.
         x = x.view(x.size(0), -1)

Cantilever/setup.py
@@ -43,7 +43,7 @@ key_image_height = 'Image Height'
 
 # Size of input images (channel-height-width). Must have the same aspect ratio as the largest possible cantilever geometry.
 INPUT_CHANNELS = 2
-INPUT_SIZE = (INPUT_CHANNELS, 250, 500)
+INPUT_SIZE = (INPUT_CHANNELS, 50, 100)
 assert (INPUT_SIZE[1] / INPUT_SIZE[2]) == (height.high / length.high), 'Input image size must match aspect ratio of cantilever: {height.high}:{length.high}.'
 # Size of output images (channel-height-width) produced by the network. Output images produced by FEA will be resized to this size.
 OUTPUT_CHANNELS = 1
@@ -229,7 +229,7 @@ def write_ansys_script(samples, filename) -> None:
         file.writelines(lines)
         print(f'Wrote {filename}.')
 
-def generate_label_images(samples, folder, normalization_values:tuple=(None,None), clip_high_stresses=False) -> Tuple[List[np.ndarray], List[float]]:
+def generate_label_images(samples, folder, normalize=False, normalization_values:tuple=(None,None), clip_high_stresses=False) -> Tuple[List[np.ndarray], List[float]]:
     """
     Return a list of images for each of the FEA text files and a list of maximum values found in each channel.
 
@@ -242,7 +242,7 @@ def generate_label_images(samples, folder, normalization_values:tuple=(None,None
     assert len(fea_filenames) == number_samples, f'Found {len(fea_filenames)} .txt files in {folder}, but should be {number_samples}.'
 
     # Store all stress data in a single array, initialized with a specific background value.
-    BACKGROUND_VALUE_INITIAL = -1
+    BACKGROUND_VALUE_INITIAL = 0 #-1
     BACKGROUND_VALUE = 0
     labels = np.full(
         (*OUTPUT_SIZE, len(fea_filenames)),
@@ -292,10 +292,11 @@ def generate_label_images(samples, folder, normalization_values:tuple=(None,None
     for channel in range(OUTPUT_CHANNELS):
         maximum = np.max(labels[channel, ...])
         maxima.append(maximum)
-        normalization_value = maximum if normalization_values[channel] is None else normalization_values[channel]
-        assert normalization_value >= maximum, f'The value by which values in channel {channel} are divided {normalization_value} is less than the maximum value found {maximum}, which will cause normalized values to be > 1.'
-        labels[channel, ...][labels[channel, ...] != BACKGROUND_VALUE_INITIAL] /= normalization_value
-        labels[labels == BACKGROUND_VALUE_INITIAL] = BACKGROUND_VALUE
+        if normalize:
+            normalization_value = maximum if normalization_values[channel] is None else normalization_values[channel]
+            assert normalization_value >= maximum, f'The value by which values in channel {channel} are divided {normalization_value} is less than the maximum value found {maximum}, which will cause normalized values to be > 1.'
+            labels[channel, ...][labels[channel, ...] != BACKGROUND_VALUE_INITIAL] /= normalization_value
+            labels[labels == BACKGROUND_VALUE_INITIAL] = BACKGROUND_VALUE
 
     return [labels[..., i] for i in range(labels.shape[-1])], tuple(maxima)
 
@@ -320,9 +321,10 @@ def hsv_to_rgb(array) -> np.ndarray:
                 array[i, j, k] = rgb[k] * 255
     return array
 
-def array_to_colormap(array) -> np.ndarray:
-    """Convert a 2D array of values in [0, 1] to a color image."""
+def array_to_colormap(array, divide_by) -> np.ndarray:
+    """Scale a 2D array of values to be inside [0, 1] and convert to a 3D color image."""
 
+    array /= divide_by
     # Invert the values so that red represents high stresses.
     array = 1 - array
     # Constrain the values so that only colors from red to blue are shown, to match standard colors used in FEA.
@@ -331,4 +333,9 @@ def array_to_colormap(array) -> np.ndarray:
     SATURATION, VALUE = 1, 2/3
     array = np.dstack((array, SATURATION * np.ones(array.shape, float), VALUE * np.ones(array.shape, float)))
     array = hsv_to_rgb(array)
+    
     return array
+
+def write_image(array, filename) -> None:
+    with Image.fromarray(array.astype(np.uint8)) as image:
+        image.save(filename)