656d0658c02ab54784c23c0b9a96b2488090c657
Marshall Lee <marsh92909@gmail.com>
2022-03-16 17:35:35 -0400

    Fix continued training on saved models
    - Changed call to model.eval() to model.train(True or False)
        - Only affects some layers like BatchNorm, Dropout
    - Added saving optimizer params (SGD), epoch #, loss to model.pth

--- Cantilever/main.py
+++ Cantilever/main.py
@@ -116,9 +116,14 @@ def test(dataloader, model, loss_function):
     print(f'Average loss: {test_loss:>8f}')
     return test_loss
 
-def save(model):
+def save(epoch, model, optimizer, loss):
     """Save model parameters to a file."""
-    torch.save(model.state_dict(), FILEPATH_MODEL)
+    torch.save({
+        'epoch': epoch,
+        'model_state_dict': model.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict(),
+        'loss': loss,
+    }, FILEPATH_MODEL)
     print(f'Saved model parameters to {FILEPATH_MODEL}.')
 
 
@@ -127,41 +132,50 @@ if __name__ == '__main__':
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     print(f'Using {device} device.')
 
-    # Initialize the model and load its parameters if it has already been trained.
+    # Initialize the model and optimizer and load their parameters if they have been saved previously.
     model = Model()
     model.to(device)
+    optimizer = torch.optim.SGD(model.parameters(), lr=LEARNING_RATE)
+    loss_function = nn.MSELoss()
+    
     train_model = True
+    epochs = range(EPOCHS)
     if os.path.exists(FILEPATH_MODEL):
-        model.load_state_dict(torch.load(FILEPATH_MODEL, map_location=torch.device(device)))
-        model.eval()
+        checkpoint = torch.load(FILEPATH_MODEL, map_location=torch.device(device))
+        model.load_state_dict(checkpoint['model_state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        epoch = checkpoint['epoch'] + 1
+        epochs = range(epoch, epoch+EPOCHS)
+        loss = checkpoint['loss']
+
         print(f'Loaded previously trained parameters from {FILEPATH_MODEL}.')
         if not input('Continue training this model? (y/n) ').lower().startswith('y'):
             train_model = False
+        
+        model.train(train_model)
     
     # Set up the training data.
     train_dataset = CantileverDataset(is_train=True)
     train_dataloader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
-    loss_function = nn.MSELoss()
-    optimizer = torch.optim.SGD(model.parameters(), lr=LEARNING_RATE)
 
     if train_model:
         # Train the model and record the accuracy and loss.
         test_loss_values = []
-        for t in range(EPOCHS):
-            print(f'Epoch {t+1}\n------------------------')
+        for epoch in epochs:
+            print(f'Epoch {epoch+1}\n------------------------')
             train(train_dataloader, model, loss_function, optimizer)
             test_loss = test(train_dataloader, model, loss_function)
             test_loss_values.append(test_loss)
             # Save the model parameters periodically.
-            if (t+1) % 5 == 0:
-                save(model)
+            if (epoch+1) % 5 == 0:
+                save(epoch, model, optimizer, test_loss)
         
         # Save the model parameters.
-        save(model)
+        save(epoch, model, optimizer, test_loss)
         
         # Plot the loss history.
         plt.figure()
-        plt.plot(test_loss_values, '-o', color='#0095ff')
+        plt.plot(epochs, test_loss_values, '-o', color='#0095ff')
         plt.xlabel('Epochs')
         plt.ylabel('Loss')
         plt.grid(axis='y')
